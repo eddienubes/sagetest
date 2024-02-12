99f6f927bb277762ffa0c7fb053c039894a08bdd
eddienubes <cool.gegeg@gmail.com>
2024-02-12 01:30:04 +0100

    feat: generated docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://eddienubes.github.io',
-  base: '/sagetest',
+  base: '/sagetest/',
   integrations: [starlight({
     title: 'Sagetest',
     favicon: 'shard-small.png',
