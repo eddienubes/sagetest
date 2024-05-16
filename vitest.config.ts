5a6c98793eed6c4e7bdee64f7e775f7a5bb0cb5e
eddienubes <cool.gegeg@gmail.com>
2024-05-16 18:29:47 +0200

    chore: verbose tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    reporters: ['verbose'],
-    disableConsoleIntercept: true,
     setupFiles: ['test/setup.ts'],
     include: ['test/**/*.spec.ts', 'examples/**/*.spec.ts'],
     coverage: {
